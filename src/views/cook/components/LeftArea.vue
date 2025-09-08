2cb1961a0c1ef0a7115010520f9de1a56874df1d
zhouzhou <is-zhou@outlook.com>
2025-09-08 17:53:37 +0800

    chore: drag test

--- src/views/cook/components/LeftArea.vue
+++ src/views/cook/components/LeftArea.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import VueDraggable from 'vuedraggable'
+type DraggableItem = {
+  label: string
+  key: string
+}
+const list = ref<DraggableItem[]>([
+  { label: '标签1', key: '1' },
+  { label: '标签2', key: '2' },
+])
+const pushContentItem = (current: DraggableItem) => {
+  return current
+}
+</script>
 
 <template>
-  <div class="left_area">左</div>
+  <div class="left_area">
+    <VueDraggable
+      v-model="list"
+      :group="{ name: 'form', pull: 'clone', put: false }"
+      :clone="pushContentItem"
+      :sort="false"
+      tag="div"
+      item-key="label"
+    >
+      <template #item="{ element }">
+        <div>
+          <div>{{ element.label }}</div>
+          <div></div>
+        </div>
+      </template>
+    </VueDraggable>
+  </div>
 </template>
 
 <style scoped lang="scss">
--- src/views/cook/components/MiddleArea.vue
+++ src/views/cook/components/MiddleArea.vue
@@ -1,7 +1,39 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref, watch } from 'vue'
+import VueDraggable from 'vuedraggable'
+type DraggableItem = {
+  label: string
+  key: string
+}
+const list = ref<DraggableItem[]>([])
+
+watch(
+  () => list.value,
+  () => {
+    console.log(list.value)
+  },
+)
+</script>
 
 <template>
-  <div class="middle_area">中</div>
+  <div class="middle_area">
+    <VueDraggable
+      v-model="list"
+      :animation="200"
+      :disabled="false"
+      group="form"
+      tag="div"
+      item-key="key"
+      class="drag_container"
+    >
+      <template #item="{ element }">
+        <div>
+          <div>{{ element.label }}</div>
+          <div></div>
+        </div>
+      </template>
+    </VueDraggable>
+  </div>
 </template>
 
 <style scoped lang="scss">
@@ -11,5 +43,11 @@
   align-items: stretch;
   justify-content: center;
   padding: 10px;
+  .drag_container {
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    padding-bottom: 20px;
+  }
 }
 </style>
