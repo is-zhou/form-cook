bacf255b01a69e0ce7a12198d1f10c9c8e34da63
zhouzhou <is-zhou@outlook.com>
2025-09-08 17:15:24 +0800

    chore: update TopArea

--- src/views/cook/components/TopArea.vue
+++ src/views/cook/components/TopArea.vue
@@ -1,7 +1,36 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>上</div>
+  <header class="top_area">
+    <div class="top_area_left">
+      <h3>FormCook</h3>
+    </div>
+    <div class="top_area_middle"></div>
+    <div class="top_area_right"></div>
+  </header>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.top_area {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #fff;
+  padding: 8px 12px 8px 16px;
+  border-bottom: 1px solid #efefef;
+}
+.top_area_left {
+  display: flex;
+  gap: 10px;
+  h3 {
+    font-weight: bold;
+    margin: 0;
+  }
+}
+.top_area_middle {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
