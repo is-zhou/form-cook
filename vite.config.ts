bc26d8bbb2a425cdc053b453750e859a9c7fe7fe
zhouzhou <is-zhou@outlook.com>
2025-09-08 16:36:28 +0800

    chore: update package and config

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,18 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueJsx(),
     vueDevTools(),
+    AutoImport({
+      imports: ['vue', 'vue-router'],
+    }),
+    Components({}),
   ],
   resolve: {
     alias: {
