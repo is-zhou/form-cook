23f05f070632f5b9018080b9f36fc0604a7dbd1b
zhouzhou <is-zhou@outlook.com>
2025-09-27 21:59:40 +0800

    chore: update row setter

--- src/setters/layout/row.ts
+++ src/setters/layout/row.ts
@@ -1,20 +1,71 @@
 import type { TSettersModuleType } from '@/types/setter.ts'
 
 const s: TSettersModuleType = {
-  'style.width': {
+  'attrs.gutter': {
+    id: '',
+    componentName: 'inputNumber',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '栅格间隔',
+    },
+    attrs: {
+      precision: 0,
+    },
+  },
+  "attrs.justify": {
+    id: '',
+    componentName: 'selectV2',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: 'flex 布局下的水平排列方式',
+    },
+    attrs: {
+      options: [
+        { label: 'start', value: 'start' },
+        { label: 'end', value: 'end' },
+        { label: 'center', value: 'center' },
+        { label: 'space-around', value: 'space-around' },
+        { label: 'space-between', value: 'space-between' },
+        { label: 'space-evenly', value: 'space-evenly' }
+      ]
+    },
+  },
+  "attrs.align": {
+    id: '',
+    componentName: 'radioGroup',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: 'flex 布局下的垂直排列方式',
+    },
+    slots: {
+      "default": {
+        componentName: "radioButton",
+        options: [
+          { label: 'top', value: 'top' },
+          { label: 'middle', value: 'middle' },
+          { label: 'bottom', value: 'bottom' },
+        ]
+      }
+    },
+    attrs: {
+
+    },
+  },
+  "attrs.tag": {
     id: '',
     componentName: 'input',
     componentType: 'form',
     formItemAttrs: {
       field: '',
-      label: '容器宽度',
+      label: '自定义元素标签',
     },
     attrs: {
-      clearable: true,
+      type: "text"
     },
-    group: 'form',
-    defaultValue: ""
-  },
+  }
 }
 
 export default s
