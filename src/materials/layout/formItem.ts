cf7aebe3f732d7c095d45c531318dd85f8defa3b
zhouzhou <is-zhou@outlook.com>
2025-10-01 10:07:09 +0800

    chore: 新增formItem物料及设置器

---
+++ src/materials/layout/formItem.ts
@@ -0,0 +1,16 @@
+import type { Material } from '@/types/material.ts'
+
+const m: Material = {
+  label: '容器FormItem',
+  icon: '',
+  materialContent: {
+    id: '',
+    componentName: 'formItem',
+    componentType: 'layout',
+    attrs: { label: "formItem" },
+    children: [],
+    style: {},
+  },
+}
+
+export default m
---
+++ src/setters/layout/formItem.ts
@@ -0,0 +1,26 @@
+import type { TSettersModuleType } from '@/types/setter.ts'
+
+const s: TSettersModuleType = {
+  'attrs.label': {
+    id: '',
+    sort: 1,
+    componentName: 'input',
+    componentType: 'form',
+    formItemAttrs: { field: '', label: '标签' },
+    attrs: {
+      clearable: true,
+    },
+    defaultValue: '',
+  },
+  'attrs.required': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: { field: '', label: '是否必填' },
+    attrs: {
+      clearable: true,
+    },
+  },
+}
+
+export default s
