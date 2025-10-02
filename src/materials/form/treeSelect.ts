565f633a9ecb664f03bbb687de61c2e5768c7fc6
zhouzhou <is-zhou@outlook.com>
2025-10-02 18:55:29 +0800

    chore: 新增树形选择物料及设置器

---
+++ src/materials/form/treeSelect.ts
@@ -0,0 +1,90 @@
+import type { Material } from '@/types/material.ts'
+
+const m: Material = {
+  label: '树形选择',
+  icon: 'IconTreeSelect',
+  materialContent: {
+    id: '',
+    componentName: 'treeSelect',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '树形选择',
+    },
+    attrs: {
+      data: [
+        {
+          value: '1',
+          label: 'Level one 1',
+          children: [
+            {
+              value: '1-1',
+              label: 'Level two 1-1',
+              children: [
+                {
+                  value: '1-1-1',
+                  label: 'Level three 1-1-1',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: '2',
+          label: 'Level one 2',
+          children: [
+            {
+              value: '2-1',
+              label: 'Level two 2-1',
+              children: [
+                {
+                  value: '2-1-1',
+                  label: 'Level three 2-1-1',
+                },
+              ],
+            },
+            {
+              value: '2-2',
+              label: 'Level two 2-2',
+              children: [
+                {
+                  value: '2-2-1',
+                  label: 'Level three 2-2-1',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          value: '3',
+          label: 'Level one 3',
+          children: [
+            {
+              value: '3-1',
+              label: 'Level two 3-1',
+              children: [
+                {
+                  value: '3-1-1',
+                  label: 'Level three 3-1-1',
+                },
+              ],
+            },
+            {
+              value: '3-2',
+              label: 'Level two 3-2',
+              children: [
+                {
+                  value: '3-2-1',
+                  label: 'Level three 3-2-1',
+                },
+              ],
+            },
+          ],
+        },
+      ]
+    },
+    defaultValue: ""
+  },
+}
+
+export default m
---
+++ src/setters/form/treeSelect.ts
@@ -0,0 +1,172 @@
+import type { TSettersModuleType } from '@/types/setter.ts'
+
+const s: TSettersModuleType = {
+  'attrs.data': {
+    id: '',
+    componentName: 'OptionsEditor',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+    },
+    attrs: {},
+  },
+  'attrs.show-checkbox': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '显示勾选框',
+    },
+    attrs: {},
+  },
+  'attrs.multiple': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '是否多选',
+    },
+    attrs: {},
+  },
+  'attrs.filterable': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '可筛选',
+    },
+    attrs: {},
+  },
+  'attrs.check-strictly': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '选择任意级别',
+    },
+    attrs: {},
+  },
+  'attrs.node-key': {
+    id: '',
+    componentName: 'input',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '整个树中唯一字段',
+    },
+    attrs: {
+      clearable: true,
+    },
+  },
+  'attrs.filter-method': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '自定义搜索方法',
+    },
+    attrs: {
+      serializeType: "function",
+      placeholder: `(query: string, item: Record<string, any>) => boolean`
+    },
+  },
+  'attrs.titles': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '自定义列表标题',
+    },
+    attrs: {
+      serializeType: "array",
+      placeholder: `[string, string]`
+    },
+  },
+  'attrs.button-texts': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '自定义按钮文案',
+    },
+    attrs: {
+      serializeType: "array",
+      placeholder: `[string, string]`
+    },
+  },
+  'attrs.render-content': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '自定义数据项渲染函数',
+    },
+    attrs: {
+      serializeType: "array",
+      placeholder: `[string, string]`
+    },
+  },
+  'attrs.props': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '数据源的字段别名',
+    },
+    attrs: {
+      serializeType: "object",
+      placeholder: `{
+  label?: string
+  key?: string
+  disabled?: string
+}`
+    },
+  },
+  'attrs.left-default-checked': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '初始状态下左侧列表的已勾选项的 key 数组',
+    },
+    attrs: {
+      serializeType: "array",
+      placeholder: `Array<string | number>`
+    },
+  },
+  'attrs.right-default-checked': {
+    id: '',
+    componentName: 'serializeInput',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '初始状态下右侧列表的已勾选项的 key 数组',
+    },
+    attrs: {
+      serializeType: "array",
+      placeholder: `Array<string | number>`
+    },
+  },
+  'attrs.validate-event': {
+    id: '',
+    componentName: 'switch',
+    componentType: 'form',
+    formItemAttrs: {
+      field: '',
+      label: '是否触发表单验证',
+    },
+    attrs: {},
+  },
+}
+
+export default s
