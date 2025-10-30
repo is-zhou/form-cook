4ab53f6d8e6347d694c4f258c78ec714dc46feb2
is-zhou <is-zhou@users.noreply.github.com>
2025-10-30 03:14:45 +0000

    deploy: c23d7390caae4b98b45f89231857dec502045425

---
+++ .vitepress/theme/components/ProfileList.vue
@@ -0,0 +1,199 @@
+<template>
+  <div data-v-ffd71362="" data-v-1762fafe="" class="VPFeatures VPHomeFeatures">
+    <div data-v-ffd71362="" class="container">
+      <div data-v-ffd71362="" class="items">
+        <div data-v-ffd71362="" class="item grid-12">
+          <div data-v-9c26f492="" data-v-ffd71362="" class="VPLink no-icon VPFeature">
+            <article data-v-9c26f492="" class="box">
+              <div class="comp_list_container">
+                <div class="info">
+                   <h2 data-v-9c26f492="" class="title name clip">组件物料</h2>
+                    <p data-v-9c26f492="" class="details">
+                    1、可以通过分类，及搜索快速找到组件物料
+                  </p>
+                  <p data-v-9c26f492="" class="details">
+                    2、拖拽、或者点击组件即可快速添加组件到画布，拖拽搭建表单结构，无需手写繁琐代码，实时预览生成效果，显著提升开发效率。
+                  </p>
+                  <div data-v-2ab91656="" class="actions" >
+                    <div data-v-2ab91656="" class="action">
+                         <span data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">组件分类</span>
+                    </div>
+                      <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">快速搜索</span>
+                      </div>
+                            <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">拖拽新增</span>
+                      </div>
+                            <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">点击新增</span>
+                      </div>
+                    </div>
+                  </div>
+                <div class="image">
+                  <img src="../../../public/comp_list.png" width="100%"></img>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div data-v-ffd71362="" class="item grid-12">
+          <div data-v-9c26f492="" data-v-ffd71362="" class="VPLink no-icon VPFeature">
+            <article data-v-9c26f492="" class="box">
+              <div class="comp_list_container">
+
+                <div class="image">
+                  <img src="../../../public/filed_edit.png" width="100%"></img>
+                </div>
+                   <div class="info">
+                   <h2 data-v-9c26f492="" class="title name clip">快速导入</h2>
+                  <p data-v-9c26f492="" class="details">
+                    1、可以直接通过添加导入字段，来快速实现表单结构；
+                  </p>
+                   <p data-v-9c26f492="" class="details">
+                    2、可以批量的修改删除字段，根据需求批量修改字段对应的组件类型；
+                  </p>
+                     <p data-v-9c26f492="" class="details">
+                    3、也可以通过后写入或粘贴JSON数据结构，并配置解析选项来解析字段，来实现批量字段导入，方便快捷。
+                  </p>
+
+                  <div data-v-2ab91656="" class="actions" >
+                    <div data-v-2ab91656="" class="action">
+                      <span data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">批量编辑</span>
+                    </div>
+                    <div data-v-2ab91656="" class="action">
+                      <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">JSON解析</span>
+                    </div>
+
+                  </div>
+                  </div>
+              </div>
+            </article>
+          </div>
+        </div>
+        <div data-v-ffd71362="" class="item grid-12">
+          <div data-v-9c26f492="" data-v-ffd71362="" class="VPLink no-icon VPFeature">
+            <article data-v-9c26f492="" class="box">
+              <div class="comp_list_container">
+                <div class="info">
+                   <h2 data-v-9c26f492="" class="title name clip">画布及Schema</h2>
+                  <p data-v-9c26f492="" class="details">
+                    1、通过点击/拖拽物料组件到画布，后会渲染组件视图，同时左侧会生成结构树；
+                  </p>
+                  <p data-v-9c26f492="" class="details">
+                    2、查看完整的表单协议代码，且可以直接修改编辑Schema然后保存；
+                  </p>
+                  <p data-v-9c26f492="" class="details">
+                    3、画布及Schema的更新修改会有操作历史，可以通过撤销及重做来选择当前步骤；
+                  </p>
+                   <p data-v-9c26f492="" class="details">
+                    3、表单预览，及生成代码，预览当前画布已完成的表单，生成代码后可以直接复制或下载。
+                  </p>
+                  <div data-v-2ab91656="" class="actions" >
+                    <div data-v-2ab91656="" class="action">
+                         <span data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">结构树</span>
+                    </div>
+                      <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">Schema操作</span>
+                      </div>
+                      <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">撤销重做</span>
+                      </div>
+                      <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">表单预览</span>
+                      </div>
+                       <div data-v-2ab91656="" class="action">
+                          <span  data-v-7a8ace81="" data-v-2ab91656=""  class="VPButton medium brand">代码生成</span>
+                      </div>
+                    </div>
+                  </div>
+                <div class="image">
+                  <img src="../../../public/canvas_schema.png" width="100%"></img>
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+          <div data-v-ffd71362="" class="item grid-12">
+          <div data-v-9c26f492="" data-v-ffd71362="" class="VPLink no-icon VPFeature">
+            <article data-v-9c26f492="" class="box">
+              <h2 data-v-9c26f492="" class="title">组件设置器</h2>
+              <p data-v-9c26f492="" class="details">
+                组件设置器，分为表单域设置器、基础设置器和组件设置器三类，表单域设置器是针对表单域的整体属性配置；基础设置器是动态，根据表单组件或者布局组件来动态匹配对应的设置器；组件设置器也是动态的根据组件取加载对应该组件的配置器选项。常用设置器如下：
+              </p>
+              <div class="setters">
+                <div>
+                  <p>校验规则</p>
+                  <img src="../../../public/setter_rule.png" alt="">
+                </div>
+                <div>
+                  <p>组件样式</p>
+                     <img src="../../../public/setter_style.png" alt="">
+                </div>
+                <div>
+                  <p>列表选项</p>
+                  <img src="../../../public/setter_options.png" alt="">
+                </div>
+                <div>
+                  <p>组件事件</p>
+                  <img src="../../../public/setter_event.png" alt="">
+                </div>
+              </div>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.comp_list_container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 30px;
+}
+.comp_list_container .info {
+  flex: 1;
+}
+.comp_list_container .info  span{
+  display: inline-block;
+
+
+}
+.comp_list_container .image {
+  flex: 1;
+  border-radius: 2%;
+  overflow: hidden;
+}
+
+.setters{
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  gap: 20px;
+  margin-top: 20px;
+}
+.setters > div{
+  flex: 1;
+  padding: 10px;
+  text-align: center;
+
+  border-radius: 2%;
+  background-color: #fff;
+  color: #000000;
+}
+
+.setters > div img {
+  width: 100%;
+  border-radius: 2%;
+  margin-top: 6px;
+  border: 1px dashed #d2d2d7;
+  padding: 2px;
+
+}
+</style>
